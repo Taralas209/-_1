8ada545c35fd820e65a7f521808bdba612c1c6fc
Alexej Zikunow <alexej.zikunow@gmail.com>
2022-09-25 08:18:19 +0200

    ex_03

---
+++ ex_02.py
@@ -0,0 +1,12 @@
+letters = 'Who keeps company with the wolf, will learn to howl.'
+template = 'w'
+exclude = 'l'
+count = 0
+
+for i in letters:
+    if i == template:
+        count = count + 1
+    if not i == exclude:
+        print(i)
+
+print('Количество символов:', count)
