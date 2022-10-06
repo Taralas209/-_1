99798ec5dbc3a8944c4929beaa161444d758810d
Alexej Zikunow <alexej.zikunow@gmail.com>
2022-10-06 11:54:14 +0200

    ex_02_03 with txt

---
+++ ex_02_03.py
@@ -0,0 +1,16 @@
+counts = dict()
+
+for line in open(r'q4_urls.txt', encoding="utf8"):
+    cline = line.strip()
+    counts[cline] = counts.get(cline, 0) + 1
+
+counts = dict(sorted(counts.items(), key=lambda x:x[1], reverse=True))
+
+
+
+out = open(r'new.txt', "w", encoding="utf8")
+
+for line in counts:
+    out.write(f"{line}; {counts[line]}\n")
+
+out.close
