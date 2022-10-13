97f71bf6cb5fa4135784870797c611845c8985a7
Alexej Zikunow <alexej.zikunow@gmail.com>
2022-10-13 11:40:26 +0200

    update with tips

--- ex03_01.py
+++ ex03_01.py
@@ -1,4 +1,6 @@
 def find_number(name):
+    # конструкцию if-else можно заменить однострочником:
+    # print(telbook.get(name, "undefined"))
     if name in telbook:
        print(f" Номер {name}:{telbook[name]}")
     else:
@@ -7,4 +9,10 @@ def find_number(name):
 telbook = {"Иван": "3916", "Катя": "1298", "Лена": "0010"}
 
 name = input("Введите имя:")
+# желательно name чистить после пользователя
+# name = input("Введите имя:").strip().capitalize()
+# функцию лучше проектировать без функций print внутри..
+# функция должна принять значение и вернуть значение
+# а мы сами уже решим, что с возвращенным значением делать
+
 find_number(name)
